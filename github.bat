adc20a1c4913d4dd3a41aafc475f02cadc8b134d
Cecil Lew <formpotato@yahoo.com>
2016-09-15 15:53:21 -0400

    first commit

---
+++ github.bat
@@ -0,0 +1,6 @@
+echo "# TreegridDemo" >> README.md
+git init
+git add *
+git commit -m "first commit"
+git remote add origin3 https://github.com/five-star-potato/TreegridDemo.git
+git push -u origin3 master
